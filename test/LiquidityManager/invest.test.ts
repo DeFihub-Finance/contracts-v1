f3b2e3a13f3566e38a27e18a144808a6206a2c01
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-06-17 16:20:34 -0300

    refactor: liquidity manager test

--- test/LiquidityManager/invest.test.ts
+++ test/LiquidityManager/invest.test.ts
@@ -1,21 +1,20 @@
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers'
-import { Slippage } from '@defihub/shared'
+import { Fees, Slippage, unwrapAddressLike } from '@defihub/shared'
 import { LiquidityManagerFixture } from './liquidity-manager.fixture'
 import {
     LiquidityManager,
     NonFungiblePositionManager,
-    StrategyManager,
     SubscriptionManager,
     TestERC20,
+    UniswapV3Pool,
 } from '@src/typechain'
-import { UniswapV2ZapHelper } from '@src/helpers'
-import { Signer, ZeroHash, parseEther } from 'ethers'
+import { UniswapV2ZapHelper, UniswapV3ZapHelper } from '@src/helpers'
+import { AddressLike, ErrorDescription, Signer, ZeroAddress, parseEther } from 'ethers'
 import { BigNumber } from '@ryze-blockchain/ethereum'
 import { ErrorDecoder } from '@src/helpers/ErrorDecoder'
 
 describe.only('LiquidityManager#invest', () => {
     const amount = parseEther('1000')
-    const halfAmount = amount * 50n / 100n
     const SLIPPAGE_BN = new BigNumber(0.01)
 
     // prices
@@ -33,19 +32,94 @@ describe.only('LiquidityManager#invest', () => {
 
     // hub contracts
     let liquidityManager: LiquidityManager
-    let strategyManager: StrategyManager
 
     // external test contracts
     let positionManagerUniV3: NonFungiblePositionManager
+    let stableBtcLpUniV3: UniswapV3Pool
+    let btcEthLpUniV3: UniswapV3Pool
 
     // global data
-    let strategyId: bigint
-    let btcEthPoolId: bigint
-    let stableBtcPoolId: bigint
     let permitAccount0: SubscriptionManager.PermitStruct
 
     // helpers
     let uniswapV2ZapHelper: UniswapV2ZapHelper
+    let uniswapV3ZapHelper: UniswapV3ZapHelper
+
+    // TODO use helper from shared
+    function getNearestUsableTick(tick: bigint, tickSpacing: bigint) {
+        return (tick / tickSpacing) * tickSpacing
+    }
+
+    async function deductFees(amount: bigint) {
+        return Fees.deductProductFee(
+            liquidityManager,
+            amount,
+            account0,
+            permitAccount0,
+            hre.ethers.provider,
+        )
+    }
+
+    async function sortTokens(tokenA: TestERC20, tokenB: TestERC20) {
+        const [addressTokenA, addressTokenB] = await Promise.all([
+            unwrapAddressLike(tokenA),
+            unwrapAddressLike(tokenB),
+        ])
+
+        return addressTokenA.toLowerCase() > addressTokenB.toLowerCase()
+            ? { token0: tokenB, token1: tokenA }
+            : { token0: tokenA, token1: tokenB }
+    }
+
+    async function isSameToken(tokenA: AddressLike, tokenB: AddressLike) {
+        const [addressTokenA, addressTokenB] = await Promise.all([
+            unwrapAddressLike(tokenA),
+            unwrapAddressLike(tokenB),
+        ])
+
+        return addressTokenA.toLowerCase() === addressTokenB.toLowerCase()
+    }
+
+    function getEncodedSwap(
+        amount: bigint,
+        outputToken: AddressLike,
+        outputTokenPrice: BigNumber,
+        protocol: 'uniswapV2' | 'uniswapV3' = 'uniswapV2',
+    ) {
+        return protocol === 'uniswapV2'
+            ? uniswapV2ZapHelper.encodeSwap(
+                amount,
+                stablecoin,
+                outputToken,
+                USD_PRICE_BN,
+                outputTokenPrice,
+                SLIPPAGE_BN,
+                liquidityManager,
+            )
+            : uniswapV3ZapHelper.encodeExactInputSingle(
+                amount,
+                stablecoin,
+                outputToken,
+                3000,
+                USD_PRICE_BN,
+                outputTokenPrice,
+                SLIPPAGE_BN,
+                liquidityManager,
+            )
+    }
+
+    function getMinOutput(amount: bigint, tokenPrice: BigNumber) {
+        // Assume its stablecoin
+        if (tokenPrice.eq(USD_PRICE_BN))
+            return Slippage.deductSlippage(amount, SLIPPAGE_BN)
+
+        const amountBn = new BigNumber(amount.toString())
+
+        return Slippage.deductSlippage(
+            BigInt(amountBn.div(tokenPrice).toFixed(0)),
+            SLIPPAGE_BN.times(2),
+        )
+    }
 
     beforeEach(async () => {
         ({
@@ -64,44 +138,74 @@ describe.only('LiquidityManager#invest', () => {
 
             // hub contracts
             liquidityManager,
-            strategyManager,
 
             // global data
-            strategyId,
             permitAccount0,
-            btcEthPoolId,
-            stableBtcPoolId,
 
             // external test contracts
             positionManagerUniV3,
+            stableBtcLpUniV3,
+            btcEthLpUniV3,
 
             // helpers
             uniswapV2ZapHelper,
+            uniswapV3ZapHelper,
         } = await loadFixture(LiquidityManagerFixture))
     })
 
-    it('should add liquidity', async () => {
-        /*
-            struct AddLiquidityV3Params {
-                address positionManager;
-                IERC20Upgradeable inputToken;
-                IERC20Upgradeable token0;
-                IERC20Upgradeable token1;
-                uint24 fee;
-                uint depositAmountInputToken;
-                bytes swapToken0;
-                bytes swapToken1;
-                uint swapAmountToken0;
-                uint swapAmountToken1;
-                int24 tickLower;
-                int24 tickUpper;
-                uint amount0Min;
-                uint amount1Min;
-            }
-        */
+    it('should add liquidity using 50/50 token0 and token1 amount proportion', async () => {
+        await stablecoin.connect(account0).mint(account0, amount)
+        await stablecoin.connect(account0).approve(liquidityManager, amount)
 
-        // TODO remove strategy creation after implementing encode swap without strategyID
-        await strategyManager
+        const halfAmount = amount * 50n / 100n
+        const halfAmountWithDeductedFees = await deductFees(halfAmount)
+
+        const [
+            { tick },
+            tickSpacing,
+            { token0 },
+            encodedSwap,
+        ] = await Promise.all([
+            stableBtcLpUniV3.slot0(),
+            stableBtcLpUniV3.tickSpacing(),
+            sortTokens(stablecoin, wbtc),
+            getEncodedSwap(halfAmountWithDeductedFees, wbtc, BTC_PRICE_BN),
+        ])
+
+        const stableIsToken0 = await isSameToken(stablecoin, token0)
+
+        const stableAmountMin = getMinOutput(halfAmountWithDeductedFees, USD_PRICE_BN)
+        const wbtcAmountMin = getMinOutput(halfAmountWithDeductedFees, BTC_PRICE_BN)
+
+        await liquidityManager
+            .connect(account0)
+            .addLiquidityV3(
+                {
+                    positionManager: positionManagerUniV3,
+                    inputToken: stablecoin,
+                    depositAmountInputToken: amount,
+
+                    fee: 3_000,
+
+                    token0: stableIsToken0 ? stablecoin : wbtc,
+                    token1: stableIsToken0 ? wbtc : stablecoin,
+
+                    swapToken0: stableIsToken0 ? '0x' : encodedSwap,
+                    swapToken1: stableIsToken0 ? encodedSwap : '0x',
+
+                    swapAmountToken0: halfAmountWithDeductedFees,
+                    swapAmountToken1: halfAmountWithDeductedFees,
+
+                    // TODO calculate tick dinamically using price
+                    tickLower: getNearestUsableTick(tick - (tick / 10n), tickSpacing),
+                    tickUpper: getNearestUsableTick(tick + (tick / 10n), tickSpacing),
+
+                    amount0Min: stableIsToken0 ? stableAmountMin : wbtcAmountMin,
+                    amount1Min: stableIsToken0 ? wbtcAmountMin : stableAmountMin,
+                },
+                permitAccount0,
+            )
+    })
             .connect(account0)
             .createStrategy({
                 dcaInvestments: [
