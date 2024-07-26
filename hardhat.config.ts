3b329204b2a0c93bee925e3a0ba8d44c39b484d5
RauppRafael <raupperino@gmail.com>
2024-07-26 12:23:04 -0300

    chore: reduced optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 400,
+                        runs: 200,
                     },
                 },
             },
