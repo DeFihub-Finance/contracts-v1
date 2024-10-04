23ff081798092eab03f01d04b631cb24de51e639
RauppRafael <raupperino@gmail.com>
2024-10-04 11:55:33 -0300

    feat: added PoolBuilder helper

---
+++ src/helpers/PoolBuilder.ts
@@ -0,0 +1,102 @@
+import { connectors, tokenAddresses } from '@defihub/shared'
+import { BigNumber, EthErrors, notEmpty } from '@ryze-blockchain/ethereum'
+import { getChainId } from '@src/helpers/chain-id'
+import { API } from '@src/helpers/API'
+
+interface PoolTokens {
+    inputToken: string
+    outputToken: string
+}
+
+export class PoolBuilder {
+    public static async buildPools(
+        swapAmountUSD: BigNumber,
+        maxPriceImpactPercentage: BigNumber,
+    ) {
+        const [
+            chainId,
+            possiblePools,
+            tokensByAddress,
+        ] = await Promise.all([
+            getChainId(),
+            PoolBuilder._generateAllPossibleUniquePools(),
+            PoolBuilder._getTokens(),
+        ])
+
+        return (
+            await Promise.all(possiblePools.map(async pool => {
+                const inputToken = tokensByAddress[pool.inputToken]
+                const outputToken = tokensByAddress[pool.outputToken]
+
+                if (!inputToken)
+                    throw new Error(`Token not found: ${ pool.inputToken }`)
+
+                if (!outputToken)
+                    throw new Error(`Token not found: ${ pool.outputToken }`)
+
+                const swap = await API.getSwapPath(
+                    chainId,
+                    pool.inputToken,
+                    pool.outputToken,
+                    BigInt(swapAmountUSD.div(inputToken.price).shiftedBy(inputToken.decimals).toFixed(0)),
+                )
+
+                if (!swap)
+                    return
+
+                const swapAmountMinusPriceImpact = swapAmountUSD.minus(swapAmountUSD.times(maxPriceImpactPercentage))
+                const expectedOutputValue = new BigNumber(swap.outputAmount.toString())
+                    .shiftedBy(-outputToken.decimals)
+                    .times(outputToken.price)
+
+                if (expectedOutputValue.lt(swapAmountMinusPriceImpact)) {
+                    console.error(`failed creating pool for: ${ inputToken.symbol } => ${ outputToken.symbol }`)
+
+                    return
+                }
+
+                return swap
+            }))
+        ).filter(notEmpty)
+    }
+
+    private static async _getTokens() {
+        const chainId = await getChainId()
+        // must force type because tokenAddresses uses as const
+        const addresses = tokenAddresses[chainId as keyof typeof tokenAddresses]
+
+        if (!addresses)
+            throw new Error(EthErrors.UNSUPPORTED_CHAIN)
+
+        return API.getTokens(Object.values(addresses).map(address => ({ chainId, address })))
+    }
+
+    private static async _generateAllPossibleUniquePools(): Promise<PoolTokens[]> {
+        const chainId = await getChainId()
+        const pairs: PoolTokens[] = []
+        const seenPairs = new Set<string>()
+        const allTokens = Object.values(tokenAddresses[chainId as keyof typeof tokenAddresses])
+
+        for (const connectorToken of connectors[chainId as keyof typeof connectors]) {
+            for (const genericToken of allTokens) {
+                if (connectorToken === genericToken)
+                    continue
+
+                const keyConnectorAsInput = `${ connectorToken }:${ genericToken }`
+                const keyConnectorAsOutput = `${ genericToken }:${ connectorToken }`
+
+                if (!seenPairs.has(keyConnectorAsInput)) {
+                    pairs.push({ inputToken: connectorToken, outputToken: genericToken })
+                    seenPairs.add(keyConnectorAsInput)
+                }
+
+                if (!seenPairs.has(keyConnectorAsOutput)) {
+                    pairs.push({ inputToken: genericToken, outputToken: connectorToken })
+                    seenPairs.add(keyConnectorAsOutput)
+                }
+            }
+        }
+
+        return pairs
+    }
+}
