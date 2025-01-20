42257a7db6464583d635e077894ec4dfb09d3e3e
RauppRafael <raupperino@gmail.com>
2025-01-20 17:30:44 -0300

    chore: moved mock prices to constants file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './dca'
 export * from './strategies.mock'
+export * from './prices.mock'
---
+++ src/constants/prices.mock.ts
@@ -0,0 +1,7 @@
+import { BigNumber } from '@ryze-blockchain/ethereum'
+
+export const USD_PRICE_BN = new BigNumber(1)
+export const BTC_PRICE = 70_000n
+export const BTC_PRICE_BN = new BigNumber(BTC_PRICE.toString())
+export const ETH_PRICE = 10_000n
+export const ETH_PRICE_BN = new BigNumber(ETH_PRICE.toString())
