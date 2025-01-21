b963b444cb3391ca4a79c8bcc16e6553dadd7a67
RauppRafael <raupperino@gmail.com>
2025-01-21 08:20:53 -0300

    chore: simplified test description

--- test/router.test.ts
+++ test/router.test.ts
@@ -8,7 +8,7 @@ import { ETH_PRICE, ETH_PRICE_BN, USD_PRICE_BN } from '@src/constants'
 import { CommandType, RoutePlanner } from '@src/helpers/RoutePlanner'
 import { PathUniswapV3 } from '@defihub/shared'
 import { expect } from 'chai'
-import { Compare } from '../src/Compare'
+import { Compare } from '@src/Compare'
 import { BigNumber } from '@ryze-blockchain/ethereum'
 
 const ONE_BILLION_ETH = parseEther('1000000000')
@@ -57,41 +57,39 @@ describe('Universal Router', () => {
         )
     })
 
-    describe('runs', () => {
-        it('works for implementation', async () => {
-            const planner = new RoutePlanner()
+    it('swaps', async () => {
+        const planner = new RoutePlanner()
 
-            expect(await weth.balanceOf(swapper)).to.be.equal(0)
-            expect(await stablecoin.balanceOf(swapper)).to.be.equal(0)
+        expect(await weth.balanceOf(swapper)).to.be.equal(0)
+        expect(await stablecoin.balanceOf(swapper)).to.be.equal(0)
 
-            planner.addCommand(
-                CommandType.V3_SWAP_EXACT_IN,
-                [
-                    await swapper.getAddress(),
-                    parseEther('1'),
-                    parseEther('1') - parseEther('0.01'),
-                    await new PathUniswapV3(
-                        weth,
-                        [{ token: stablecoin, fee: 3000 }],
-                    ).encodedPath(),
-                    false,
-                ],
-            )
+        planner.addCommand(
+            CommandType.V3_SWAP_EXACT_IN,
+            [
+                await swapper.getAddress(),
+                parseEther('1'),
+                parseEther('1') - parseEther('0.01'),
+                await new PathUniswapV3(
+                    weth,
+                    [{ token: stablecoin, fee: 3000 }],
+                ).encodedPath(),
+                false,
+            ],
+        )
 
-            await weth.connect(swapper).mint(swapper, parseEther('1'))
-            await weth.connect(swapper).transfer(universalRouter, parseEther('1'))
+        await weth.connect(swapper).mint(swapper, parseEther('1'))
+        await weth.connect(swapper).transfer(universalRouter, parseEther('1'))
 
-            await universalRouter.connect(swapper)['execute(bytes,bytes[])'](
-                planner.commands,
-                planner.inputs,
-            )
+        await universalRouter.connect(swapper)['execute(bytes,bytes[])'](
+            planner.commands,
+            planner.inputs,
+        )
 
-            expect(await weth.balanceOf(swapper)).to.be.equal(0)
-            Compare.almostEqualPercentage({
-                value: await stablecoin.balanceOf(swapper),
-                target: parseEther('1') * ETH_PRICE,
-                tolerance: new BigNumber(0.01),
-            })
+        expect(await weth.balanceOf(swapper)).to.be.equal(0)
+        Compare.almostEqualPercentage({
+            value: await stablecoin.balanceOf(swapper),
+            target: parseEther('1') * ETH_PRICE,
+            tolerance: new BigNumber(0.01),
         })
     })
 })
