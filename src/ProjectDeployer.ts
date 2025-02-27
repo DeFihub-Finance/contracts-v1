ad2f2cb803f4d3a5b423930bdefcf986cd395008
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-02-27 14:33:33 -0300

    refactor: use deployer account

--- src/ProjectDeployer.ts
+++ src/ProjectDeployer.ts
@@ -58,7 +58,7 @@ export class ProjectDeployer {
         // Originally USDC uses 6 decimals, that's why the name choice
         const usdc = await new TestERC20__factory(deployer).deploy(6)
         const weth = await new TestERC20__factory(deployer).deploy(18)
-        const wbtc = await new TestERC20__factory(account0).deploy(18)
+        const wbtc = await new TestERC20__factory(deployer).deploy(18)
         const { factoryUniV2, routerUniV2 } = await this.deployUniV2(deployer, weth)
         const {
             factoryUniV3,
