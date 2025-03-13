7170f335aa06af5f9550eda8d2bc815234064f87
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-03-13 20:32:08 -0300

    feat: collect referrer rewards

--- contracts/StrategyManager__v2.sol
+++ contracts/StrategyManager__v2.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.26;
 
+import {IERC20Upgradeable, SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
 import {StrategyInvestor} from "./abstract/StrategyInvestor.sol";
 import {StrategyPositionManager} from "./abstract/StrategyPositionManager.sol";
 import {SubscriptionManager} from "./SubscriptionManager.sol";
@@ -10,7 +12,10 @@ import {ReferralStorage} from "./libraries/ReferralStorage.sol";
 
 // TODO test upgrade compatibility
 contract StrategyManager__v2 is StrategyManager {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
     event Referral(address referrer, address referred);
+    event CollectedReferrerRewards(address referrer, uint amount);
 
     // TODO add reinitializer for referrerPercentage
 
@@ -62,5 +67,16 @@ contract StrategyManager__v2 is StrategyManager {
 
     function getReferrerRewards(address _referrer) external virtual view returns (uint) {
         return ReferralStorage.getReferralStruct().referrerRewards[_referrer];
+    }
+
+    function collectReferrerRewards() public virtual {
+        ReferralStorage.ReferralStruct storage referralStorage = ReferralStorage.getReferralStruct();
+
+        uint referrerReward = referralStorage.referrerRewards[msg.sender];
+
+        referralStorage.referrerRewards[msg.sender] = 0;
+        stable.safeTransfer(msg.sender, referrerReward);
+
+        emit CollectedReferrerRewards(msg.sender, referrerReward);
     }
 }
