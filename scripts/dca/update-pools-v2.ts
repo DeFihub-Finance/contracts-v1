fca44be3eee73c49fef38c15b63310133c12d277
RauppRafael <raupperino@gmail.com>
2025-04-10 15:12:47 -0300

    chore: removed "routerToQuoter" map

--- scripts/dca/update-pools-v2.ts
+++ scripts/dca/update-pools-v2.ts
@@ -1,7 +1,7 @@
 import { createArrayOfIndexes, ERC20PricedJson, ERC20JsonAddressMap, exchangesMeta } from '@defihub/shared'
 import { DollarCostAverage__factory, Quoter__factory } from '@src/typechain'
 import { API, findAddressOrFail, getChainId, getSigner, proposeTransactions } from '@src/helpers'
-import { BatchLimiter, BigNumber, ChainId, ChainIds, ChainMap, notEmpty } from '@ryze-blockchain/ethereum'
+import { BatchLimiter, BigNumber, ChainId, notEmpty } from '@ryze-blockchain/ethereum'
 import { PreparedTransactionRequest, Signer } from 'ethers'
 
 type Pool = Awaited<ReturnType<typeof getPools>>[number]
@@ -20,24 +20,6 @@ type TokenMap = ERC20JsonAddressMap<ERC20PricedJson>
 const limiter = new BatchLimiter(10, 1_000)
 const INPUT_AMOUNT_USD = new BigNumber(1_000) // $1.000
 const MAX_SLIPPAGE = 0.01 // 1%
-
-const routerToQuoter: ChainMap<Record<string, string>> = {
-    [ChainIds.ARBITRUM]: {
-        '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45': '0xb27308f9f90d607463bb33ea1bebb41c27ce5ab6', // uni router02
-        '0xa51afafe0263b40edaef0df8781ea9aa03e381a3': '0xb27308f9f90d607463bb33ea1bebb41c27ce5ab6', // uni universal
-
-        '0x1b81d678ffb9c0263b24a97847620c99d213eb14': '0xb048bbc1ee6b733fffcfb9e9cef7375518e25997', // pancake router02
-        '0x13f4ea83d0bd40e75c8222255bc855a974568dd4': '0xb048bbc1ee6b733fffcfb9e9cef7375518e25997', // pancake universal
-    },
-    [ChainIds.BNB]: {
-        '0xb971ef87ede563556b2ed4b1c0b0019111dd85d2': '0x78d78e420da98ad378d7799be8f4af69033eb077', // uni router02
-        '0x1906c1d672b88cd1b9ac7593301ca990f94eae07': '0x78d78e420da98ad378d7799be8f4af69033eb077', // uni universal
-
-        '0x13f4ea83d0bd40e75c8222255bc855a974568dd4': '0xb048bbc1ee6b733fffcfb9e9cef7375518e25997', // pancake smart
-        '0x1b81d678ffb9c0263b24a97847620c99d213eb14': '0xb048bbc1ee6b733fffcfb9e9cef7375518e25997', // pancake router02
-        '0x32226588378236fd0c7c4053999f88ac0e5cac77': '0xb048bbc1ee6b733fffcfb9e9cef7375518e25997', // pancake universal
-    },
-}
 
 let chainId: ChainId
 let signer: Signer
@@ -203,13 +185,19 @@ async function getQuotes(pool: Pool): Promise<{ original: PoolWithSwapData, upda
     }
 }
 
+function routerToQuoter(router: string) {
+    const quoter = exchangesMeta[chainId]
+        ?.find(exchange => exchange.router === router)
+        ?.quoter
+
+    if (!quoter)
+        throw new Error(`Quoter not found for router ${ router }`)
+
+    return quoter
+}
+
 async function getQuote(router: string, path: string, inputAmount: bigint) {
-    const quoterAddress = routerToQuoter[chainId]?.[router]
-
-    if (!quoterAddress)
-        throw new Error(`Quoter not found ${ router }`)
-
-    const quoter = Quoter__factory.connect(quoterAddress, signer)
+    const quoter = Quoter__factory.connect(routerToQuoter(router), signer)
 
     try {
         await limiter.consumeLimit()
