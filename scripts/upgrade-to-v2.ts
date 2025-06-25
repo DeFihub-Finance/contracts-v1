aa8c09a8daa74e6c36cfb28421bee58dd0438960
RauppRafael <raupperino@gmail.com>
2025-06-25 09:35:32 -0300

    feat: added script to upgrade contract to v2

---
+++ scripts/upgrade-to-v2.ts
@@ -0,0 +1,36 @@
+import { deployImplementation, findAddressOrFail } from '@src/helpers'
+import { upgrade } from '@src/helpers/upgrade'
+import { StrategyInvestor__factory, StrategyManager__v2__factory, StrategyPositionManager__factory } from '@src/typechain'
+
+async function upgradeToV2() {
+    const strategyInvestor = await deployImplementation(
+        'StrategyInvestor',
+        StrategyInvestor__factory.bytecode,
+    )
+    const strategyPositionManager = await deployImplementation(
+        'StrategyPositionManager',
+        StrategyPositionManager__factory.bytecode,
+    )
+
+    await upgrade(
+        await findAddressOrFail('StrategyManager'),
+        'StrategyManager__v2',
+        StrategyManager__v2__factory
+            .createInterface()
+            .encodeFunctionData(
+                'initialize__v2',
+                [
+                    strategyInvestor,
+                    strategyPositionManager,
+                    10n,
+                ],
+            ),
+    )
+
+    await upgrade(
+        await findAddressOrFail('LiquidityManager'),
+        'LiquidityManager',
+    )
+}
+
+upgradeToV2()
