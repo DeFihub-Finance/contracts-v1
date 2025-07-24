be3a78b6129009062a2e4ce0c46759fe095e00f7
RauppRafael <raupperino@gmail.com>
2025-07-23 22:11:45 -0400

    feat: added check to prevent referrals of users that aren't new

--- contracts/libraries/ReferralStorage.sol
+++ contracts/libraries/ReferralStorage.sol
@@ -6,6 +6,7 @@ library ReferralStorage {
     bytes32 constant private REFERRAL_STORAGE_POSITION = keccak256("referral.storage");
 
     struct ReferralStruct {
+        mapping(address => bool) investedBefore;
         mapping(address => address) referrals;
         mapping(address => uint) referrerRewards;
         uint32 referrerPercentage;
