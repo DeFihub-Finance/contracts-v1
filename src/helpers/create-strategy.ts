fcf55a654280462cced3fd8f4d34d6decbb0c639
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-09-06 12:40:34 -0300

    test: collect rewards

--- src/helpers/create-strategy.ts
+++ src/helpers/create-strategy.ts
@@ -1,27 +1,28 @@
-import { StrategyManager, SubscriptionManager } from '@src/typechain'
-import { Signer, ZeroHash } from 'ethers'
+import { StrategyManager, StrategyManager__v4, SubscriptionManager } from '@src/typechain'
+import { BigNumberish, Signer, ZeroHash } from 'ethers'
 
 export async function createStrategy(
     strategist: Signer,
     permit: SubscriptionManager.PermitStruct,
-    strategyManager: StrategyManager,
+    strategyManager: StrategyManager__v4,
     {
         dcaInvestments,
         vaultInvestments,
         liquidityInvestments,
         buyInvestments,
     }: Omit<StrategyManager.CreateStrategyParamsStruct, 'permit' | 'metadataHash'>,
+    liquidityRewardFeeBP?: BigNumberish,
 ): Promise<bigint> {
     const strategyId = await strategyManager.getStrategiesLength()
 
-    await strategyManager.connect(strategist).createStrategy({
+    await strategyManager.connect(strategist).createStrategyV2({
         dcaInvestments,
         vaultInvestments,
         liquidityInvestments,
         buyInvestments,
         permit,
         metadataHash: ZeroHash,
-    })
+    }, liquidityRewardFeeBP ?? 0)
 
     return strategyId
 }
